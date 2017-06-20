02aa7649c23056abe33240d61a75bbf40aaaa4b1
Sumin Lee <suminlee0610@naver.com>
2017-06-21 07:24:04 +0900

    Refactored the code into model view controller and enemies attack power now dictates how much health points the player loose

---
+++ src/model/Gunner.java
@@ -0,0 +1,30 @@
+package model;
+
+import javax.swing.ImageIcon;
+
+public class Gunner extends Character{
+	
+	private static Gunner instance = null;
+	
+	public synchronized static Gunner getInstance() {
+		if (instance == null) {
+			instance = new Gunner();
+		}
+		return instance;
+	}
+	
+	private Gunner(){
+		super();
+		
+		characterClass = "gunner";
+		characterRight = new ImageIcon("./images/" + characterClass + "_right.png");
+    	characterJump = new ImageIcon("./images/" + characterClass + "_right.png");
+    	characterLeft = new ImageIcon("./images/" + characterClass + "_left.png");
+    	imgLeft = characterRight.getImage(); //캐릭터 이미지 불러옴
+
+    	
+		hp = 200;
+		ammo = 10;
+		attackPower = 12;
+	}
+}
